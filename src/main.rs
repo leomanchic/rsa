e5d5f37ffdb2a3efe1947c4100d1df32ebcbee33
Denis Bykov <z.bykov.d@gmail.com>
2023-09-19 00:58:22 +0300

    add serialization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use num_bigint::{BigInt, RandBigInt};
-use num_traits::{One, Zero};
-use std::str;
 use num_integer::Integer;
+use num_traits::{One, Zero};
+use std::fs::File;
+use std::io::{Read, Write};
+use std::str;
 
 fn is_prime(n: &BigInt, k: i32) -> bool {
     if *n <= BigInt::one() {
@@ -92,7 +94,7 @@ fn mod_inverse(e: &BigInt, phi: &BigInt) -> Option<BigInt> {
 }
 
 fn main() {
-    let num_bits = 64;
+    let num_bits = 1024;
 
     let large_prime = generate_large_prime(num_bits);
 
@@ -114,6 +116,7 @@ fn main() {
     println!("Public key is {}", e);
 
     let d = mod_inverse(&e, &phi_n).expect("Error");
+    println!("LENYA LOX is {}", d);
 
     let mut text = "Hello World".as_bytes();
     println!("{:?}", text);
@@ -123,12 +126,72 @@ fn main() {
         .map(|x| BigInt::from(*x as i32).modpow(&e, &n))
         .collect();
 
-    println!("encrypted: {:?}", encrypted);
+    // println!("encrypted: {:?}", encrypted);
 
-    let decrypted: Vec<BigInt> = encrypted.into_iter().map(|x| x.modpow(&d, &n)).collect();
+    let mut peredacha: Vec<u8> = Vec::new();
+    for i in encrypted {
+        let (sign, mut v) = i.to_bytes_le();
+        peredacha.push(match sign {
+            num_bigint::Sign::Minus => 0u8,
+            num_bigint::Sign::NoSign => 1,
+            num_bigint::Sign::Plus => 2,
+        });
+        let size = (v.len() as u16).to_le_bytes();
+        peredacha.extend_from_slice(&size);
+        peredacha.append(&mut v);
+    }
 
-    for i in decrypted{
-        println!("{:?}",str::from_utf8(&i.to_bytes_be().1).unwrap());
+    let mut file = File::create("peredacha.bin").unwrap();
+    file.write_all(&peredacha).unwrap();
+
+    let mut file = File::open("peredacha.bin").unwrap();
+    let mut peredacha = vec![];
+    file.read_to_end(&mut peredacha).unwrap();
+    // file.write_all(&peredacha).unwrap();
+
+    let mut prinyli: Vec<BigInt> = vec![];
+    let mut ptr = 0usize;
+    let mut zakonchil: bool = false;
+    while !zakonchil {
+        let sign = peredacha[ptr];
+        println!("{}", sign);
+        let sign = match sign {
+            0 => num_bigint::Sign::Minus,
+            1 => num_bigint::Sign::NoSign,
+            2 => num_bigint::Sign::Plus,
+            _ => panic!("Gde to ya proebalsya"),
+        };
+        ptr += 1;
+        // println!("ptr {ptr}");
+        let size_bytes = &peredacha[ptr..ptr + 2];
+        let mut size_bytes2 = [0u8; 2];
+        for (idx, byte) in size_bytes.iter().enumerate() {
+            size_bytes2[idx] = *byte;
+        }
+        ptr += 2;
+        // println!("ptr {ptr}");
+        let size = u16::from_le_bytes(size_bytes2);
+        // println!("size {size}");
+        // println!("ptr {ptr}");
+        let bytes = &peredacha[ptr..(ptr + size as usize)];
+        ptr += size as usize;
+        let bigint = BigInt::from_bytes_le(sign, bytes);
+        prinyli.push(bigint);
+        // println!("converted to bigint");
+        // ptr += 1;
+        // println!("ptr {ptr}");
+        if ptr == peredacha.len() {
+            zakonchil = true;
+        }
+    }
+
+    // [0][1]   [3]      [11][12]    [14]
+    // [0][1..2][3.. 3+8][11][12..13][14.. 14 + 6]
+
+    let decrypted: Vec<BigInt> = prinyli.into_iter().map(|x| x.modpow(&d, &n)).collect();
+
+    for i in decrypted {
+        println!("{:?}", str::from_utf8(&i.to_bytes_be().1).unwrap());
     }
 
     // println!("decrypted: {:?}", decrypted);
