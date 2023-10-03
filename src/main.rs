debd5a7c4ce1269a1be89ba016d61d5cbbe9439e
leomanchic <perleomix@gmail.com>
2023-10-03 17:58:55 +0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::Parser;
-use rsa;
+use rsamixed::blowfish::blowcrypt;
 use std::fs::File;
 use std::io::{Read, Write};
 
 #[derive(Parser)]
-    struct Cli {
+struct Cli {
     /// Bit length of keys
     #[arg(short, long, value_name = "len_of_key")]
     bit_len: u32,
@@ -14,27 +14,43 @@ use std::io::{Read, Write};
     messege: String,
 }
 
-use std::str;
 fn main() {
+    let key ="verysecretpasswd".as_bytes();
+    let bf = blowcrypt::Blowfish::new(key).unwrap();
+
+    let mut txt = b"gdhdfjfg".to_vec();
+    println!("{:02x?}", txt);
+
+    bf.encrypt_block((&mut txt[..8]).try_into().unwrap());
+
+    println!("{:02x?}", txt);
+   
+   
+
     let args = Cli::parse();
     let text = args.messege.as_bytes();
     println!("{:?}", text);
 
-    let key_xor = "leonid".as_bytes();
-    let text = rsa::xor(key_xor, text);
+    // let key_xor = "leonid".as_bytes();
+    // let text = rsamixed::rsa::rsacrypt::xor(key_xor, text);
 
     println!("{:?}", text);
 
-    let (e, d, n) = rsa::processing(args.bit_len).unwrap();
+    let (e, d, n) = rsamixed::rsa::rsacrypt::processing(args.bit_len).unwrap();
 
-    let enc = rsa::encryptinon(&e, &n, key_xor).unwrap();
+    let enc = rsamixed::rsa::rsacrypt::encryptinon(&e, &n, key).unwrap();
     // println!("{:?}", enc);
 
-    let seria = rsa::serialization(enc).unwrap();
-    let desir = rsa::deseriallization(seria).unwrap();
-    let decr = rsa::decryption(desir, d, n).unwrap();
+    let seria = rsamixed::rsa::rsacrypt::serialization(enc).unwrap();
+    let desir = rsamixed::rsa::rsacrypt::deseriallization(seria).unwrap();
+    let decr = rsamixed::rsa::rsacrypt::decryption(desir, d, n).unwrap();
 
     let decrypted_xor_key: Vec<u8> = decr.into_iter().map(|x| x.to_bytes_be().1[0]).collect();
-    let text_decryption = rsa::xor(&decrypted_xor_key, &text);
-    println!("{:?}", text_decryption)
+    // let text_decryption = rsamixed::rsa::rsacrypt::xor(&decrypted_xor_key, &text);
+    let bf = blowcrypt::Blowfish::new(&decrypted_xor_key).unwrap();
+
+    bf.decrypt_block((&mut txt[..8]).try_into().unwrap());
+    println!("{:02x?}", txt);
+
+    // println!("{:?}", text_decryption)
 }
